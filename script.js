0f10ba6843e2987a86d683fa2b9f8826554c4246
fahad <fahad@gmail.com>
2026-04-25 15:07:52 +0600

    completed all requirements and it is ready

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 
 const callHistoryData = [];
 
+// funtion to get innertext
+function getInnerText(value){
+    const innerText = parseInt(document.getElementById(value).innerText);
+    return innerText; 
+}
+// function to set innertext
+function setInnertext(id, value){
+    document.getElementById(id).innerText = value;
+} 
 // Heart count function 
 const hearts = document.getElementsByClassName("click-heart");
 
 for(const heart of hearts){
     heart.addEventListener("click",function(){
-        const heartCount = parseInt(document.getElementById("heart-counter").innerText);
+        const heartCount = getInnerText("heart-counter");
 
         const newCount = heartCount + 1;
 
-        document.getElementById("heart-counter").innerText = newCount;
+        setInnertext("heart-counter", newCount);
+
     })
 }
 
@@ -20,7 +30,7 @@ const buttons = document.getElementsByClassName("call-button");
 for(const button of buttons){
     button.addEventListener("click",function(){
 
-    const coinCount = parseInt(document.getElementById("coin-counter").innerText);
+    const coinCount = getInnerText("coin-counter");
 
     const card = button.closest(".cards");
 
@@ -37,7 +47,7 @@ for(const button of buttons){
 
     const newCoinCount = coinCount - 20;
 
-    document.getElementById("coin-counter").innerText = newCoinCount;
+    setInnertext("coin-counter", newCoinCount);
 
     const data = {
         title : subTitle,
@@ -53,11 +63,34 @@ for(const button of buttons){
 
 }
 
+// clear button function
 document.getElementById("clear-button").addEventListener("click",function(){
     callHistoryData.length = 0;
     document.getElementById("history-container").innerHTML = "";
 })
 
+// copy button function
+const boxs = document.getElementsByClassName("copy-box");
+for(const box of boxs){
+    box.addEventListener("click",function(){
+    
+    const card = box.closest(".cards");
+
+    const number = card.querySelector(".card-number").textContent;
+
+    navigator.clipboard.writeText(number);
+
+    alert("Number copied"+" "+number);
+
+    const copyCount = getInnerText("copy-counter");
+
+    const newCount = copyCount + 1;
+
+    setInnertext("copy-counter", newCount);
+    // document.getElementById("copy-counter").innerHTML = newCount;
+})
+}
+// show history function
 function showHistory(){
     const historyContainer = document.getElementById("history-container");
 
